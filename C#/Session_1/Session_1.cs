1878030a90aa61d7f5009267a7ae70ea034f9976
Mohamed Zakaria <132154144+moziko00@users.noreply.github.com>
2023-05-08 23:35:18 +0300

    Assignment

---
+++ C#/Session_1/Session_1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+
+namespace Assignment_Sesssion_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Number 1
+
+            //Console.WriteLine("Please Enter Any Number U Want ");
+            //int x = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(x); 
+            #endregion
+
+            #region Number 2
+            //Console.WriteLine("Please Enter The Number ");
+            //int X= int.Parse(Console.ReadLine());
+            //if ((X % 3 == 0) && (X % 4 == 0))
+            //    Console.WriteLine("Yes");
+            //else
+            //    Console.WriteLine("NO"); 
+            #endregion
+
+            #region Number3
+            //Console.WriteLine("Please Enter Two Intger Numbers");
+            //int x=int.Parse(Console.ReadLine());
+            //int y=int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(Math.Max(x,y)); 
+            #endregion
+
+            #region Number 4    
+            //Console.WriteLine("Please Enter The Number");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x < 0)
+            //    Console.WriteLine("Negative");
+            //else
+            //    Console.WriteLine("Positive"); 
+            #endregion
+
+            #region Number 5
+            //Console.WriteLine("Please Enter Three Numbers");
+            //int x = int.Parse(Console.ReadLine());
+            //int y = int.Parse(Console.ReadLine());
+            //int z = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Max Element={Math.Max(Math.Max(x, y), z)}");
+            //Console.WriteLine($"Min Element={Math.Min(Math.Min(x, y), z)}"); 
+            #endregion
+
+            #region Number 6
+            //Console.WriteLine("Please Enter The Number");
+            //int x=int.Parse(Console.ReadLine());
+            //if (x % 2 == 1)
+            //    Console.WriteLine("Odd");
+            //if (x % 2 == 0)
+            //    Console.WriteLine("Even"); 
+            #endregion
+
+            #region Number 7
+            //Console.WriteLine("Please Enter Any Character");
+            //char x=char.Parse(Console.ReadLine());
+
+            //if (x == 'a' || x == 'e' || x == 'I' || x == 'o' || x == 'u')
+            //    Console.WriteLine("vowel");
+            //else
+            //    Console.WriteLine("consonant"); 
+            #endregion
+
+            #region Number 12
+            //Console.WriteLine("Please Enter Marks Of Five Subjects");
+            //int x=int.Parse(Console.ReadLine());
+            //int y=int.Parse(Console.ReadLine());
+            //int z=int.Parse(Console.ReadLine());
+            //int m=int.Parse(Console.ReadLine());
+            //int n=int.Parse(Console.ReadLine());
+            //int Sum=n+y+z+m+n;
+            //float Avarage=Sum/5;
+            //float Percentage = Avarage * 100;
+
+
+            //Console.WriteLine($"Total Marks ={Sum}");
+            //Console.WriteLine($"Average Marks ={Avarage}");
+            //Console.WriteLine($"Percentage ={Percentage} %"); 
+            #endregion
+
+            #region Number 13
+            //Console.WriteLine("Please Enter The Month Number");
+            //int month=int.Parse(Console.ReadLine());
+
+            //switch (month)
+            //{
+            //    case 1:
+            //    case 3:
+            //    case 5:
+            //    case 7:
+            //    case 8:
+            //    case 10:
+            //    case 12:
+            //        Console.WriteLine("This Month  Have 31 Days.");
+            //        break;
+
+            //    case 2:
+            //        Console.WriteLine("This Month Have 28 Days.");
+            //        break;
+
+            //    case 4:
+            //    case 6:
+            //    case 9:
+            //    case 11:
+            //        Console.WriteLine("This Month Have 30 Days");
+            //            break;
+            //}
+            #endregion
+
+            #region Number 14
+            //Console.WriteLine("Please enter the marks of  Physics");
+            //int x= int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please enter the marks of  Chemistry");
+            //int y= int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please enter the marks of  Biology");
+            //int z= int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please enter the marks of  Mathematics");
+            //int m= int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please enter the marks of  Computer");
+            //int n= int.Parse(Console.ReadLine());
+
+            //int total = n + m + y + z + x;
+            //double per = total / 5.0;
+            //if (per >= 90)
+            //    Console.WriteLine("Grad A");
+            //else if (per >=80)
+            //    Console.WriteLine("Grad B");
+
+            //else if (per >= 70)
+            //    Console.WriteLine("Grad C");
+
+            //else if (per >= 60)
+            //    Console.WriteLine("Grad D");
+            //else if (per < 40)
+            //    Console.WriteLine("Grad F");
+            #endregion
+
+            #region Number 15
+            //Console.WriteLine("Please Enter Whether Number");
+            //int Deg=int.Parse(Console.ReadLine());
+            //switch (Deg)
+            //{
+            //    case  > 0:
+            //        Console.WriteLine("Positive");
+            //        break;
+
+            //    case < 0:
+            //        Console.WriteLine("Negative");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Equal Zero");
+            //        break;
+            //} 
+            #endregion
+
+            #region Number 16
+            //Console.WriteLine("Please Enter The Frist Number");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please Enter The Second Number");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please Enter the operator");
+            //char z =char.Parse(Console.ReadLine());
+            //double sum;
+            //switch(z)
+            //{
+            //    case '+':
+            //        sum = x + y;
+            //        break;
+            //    case '-':
+            //        sum = x - y;
+            //        break;
+            //    case '*':
+            //        sum = x * y;
+            //        break;
+
+            //        case '/':
+            //        if (y % 2 == 0)
+            //        {
+            //            Console.WriteLine("Can't Divide by zero");
+            //            break;
+            //        }
+            //        else
+            //        {
+            //            sum= x / y;
+            //            break;
+            //        }
+            //    default:
+            //        Console.WriteLine("Invalid Number");
+            //        break;
+            //} 
+            #endregion
+        }
+    }
+}
