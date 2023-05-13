dce84f09aa0d101e31c226b4a1b96f80dc97303e
Mohamed Zakaria <132154144+moziko00@users.noreply.github.com>
2023-05-13 21:31:38 +0300

    Assignment

---
+++ C#/Session_2/Session_1_2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Session_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Number 8
+            //Console.WriteLine("Please Enter The Number");
+            //int x = int.Parse(Console.ReadLine());
+            //for (int i = x; i > 0; i--)
+            //    Console.WriteLine(i); 
+            #endregion
+
+            #region Number 9
+
+            //Console.WriteLine("Please Enter The Number");
+            //int x = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= 12; i++)
+            //    Console.WriteLine(i * x); 
+            #endregion
+
+            #region Number 10
+
+            //Console.WriteLine("Please Enter The Number");
+            //int x= int.Parse(Console.ReadLine());
+            //while (x > 0)
+            //{
+            //    if (x % 2 == 0)
+            //        Console.WriteLine(x);
+            //    x--; 
+            //}
+            #endregion
+
+            #region Number 11
+            //Console.WriteLine("Please Enter The Frist Number");
+            //double X=int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please Enter The Power");
+            //double Y=int.Parse(Console.ReadLine());
+            //Console.WriteLine(Math.Pow(X, Y)); 
+            #endregion
+
+            #region Number 17
+            //string word = "";
+            //string reverse = "";
+            //int length = 0;
+            //Console.WriteLine("PLease Enter Any Word ");
+            //word = Console.ReadLine();
+            //length = word.Length - 1;
+            //while (length >= 0)
+            //{
+            //    reverse = reverse + word[length];
+            //    length--;
+            //}
+            //Console.WriteLine(reverse); 
+            #endregion
+
+            #region Number 18
+            //Console.WriteLine("Please Enter The Numbers");
+            //int numbers = int.Parse(Console.ReadLine());
+            //int reverse = 0;
+            //while (numbers > 0)
+            //{
+            //    int remainder = numbers % 10;
+            //    reverse = (reverse * 10) + remainder;
+            //    numbers = numbers / 10;
+            //}
+            //Console.WriteLine(reverse); 
+            #endregion
+        }
+    }
+}
